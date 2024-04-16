b9cb045c1730ffdcf03ddf1fda99df5afa01b212
Bin Shawky <108636071+ahmed14527@users.noreply.github.com>
2024-04-16 03:34:18 +0200

    added

---
+++ ProductsApp/models.py
@@ -0,0 +1,13 @@
+
+from django.db import models
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=225)
+    description = models.TextField()
+    price = models.IntegerField()
+    image = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
